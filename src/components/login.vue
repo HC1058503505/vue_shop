2d3a92daa79b3fd01b785eb0e7e04e0adf24d39e
HC1058503505 <1058503505@qq.com>
2019-11-27 17:12:25 +0800

    添加login组件

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login_com">
+        Login组件
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scoped>
+.login_com {
+    background-color: red;
+}
+</style>
