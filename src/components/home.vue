fb4224e928e6e3ce0b013d7d4cb45d49b8c2e89c
HC1058503505 <1058503505@qq.com>
2019-12-03 23:37:23 +0800

    侧边栏导航

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,13 +2,40 @@
     <div class="home_component">
       <el-container class="el_container">
         <el-header class="el_header">
-          <div>
+          <div class='nav_header'>
             <img src="../assets/imgs/gushicijianshang_pink.png" alt="诗词文斋">
             <span>诗词管理系统</span>
           </div>
+          <el-button type="info" @click="logout">
+            退出
+          </el-button>
         </el-header>
         <el-container>
-          <el-aside width="200px" class="el_aside">Aside</el-aside>
+          <el-aside width="200px" class="el_aside">
+            <el-menu  default-active="2"
+                      class="el-menu-vertical-demo"
+                      @open="handleOpen"
+                      @close="handleClose"
+                      background-color="#545c64"
+                      text-color="#fff"
+                      active-text-color="#ffd04b"
+                      :index="menu.id"
+                      :key="menu.id"
+                      v-for="menu in menulists">
+              <el-submenu>
+                <template slot="title">
+                  <i :class="iconsObj[menu.id]"></i>
+                  <span>{{ menu.authName }}</span>
+                </template>
+                <el-menu-item :index="sub.id" v-for="sub in menu.children" :key="sub.id">
+                  <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>{{ sub.authName }}</span>
+                  </template>
+                </el-menu-item>
+              </el-submenu>
+            </el-menu>
+          </el-aside>
           <el-main class="el_main">
             <router-view></router-view>
           </el-main>
@@ -20,10 +47,35 @@
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      menulists: [],
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      }
+    }
+  },
+  async created () {
+    const { data: res } = await this.$http.get('menus')
+    if (res.meta.status !== 200) {
+      return
+    }
+    this.menulists = res.data
+  },
   methods: {
     logout: function () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    handleOpen: function () {
+
+    },
+    handleClose: function () {
+
     }
   }
 }
@@ -40,17 +92,23 @@ export default {
 
 .el_header {
   background-color: #373d41;
-  display: flex;
+  display: inline-flex;
   display: -webkit-flex;
   justify-content: space-between;
-  img {
-    height: auto;
-    width: 80px;
-  }
+  align-items: center;
+  .nav_header {
+    display: inline-flex;
+    align-items: center;
+    img {
+      height: 50px;
+      width: 50px;
+    }
 
-  span {
-    color: aliceblue;
-    font-size: 20px;
+    span {
+      padding-left: 20px;
+      color: aliceblue;
+      font-size: 20px;
+    }
   }
 }
 
@@ -60,5 +118,9 @@ export default {
 
 .el_main {
   background-color: antiquewhite;
+}
+
+.iconfont {
+  margin-right: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ import axios from 'axios'
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 请求前统计设置
+// 此处保证所有请求传入token，保证请求有效
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
